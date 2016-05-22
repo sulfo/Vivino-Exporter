67a47a71841c53e78ee8c1536357211fdbda3139
Michael Sørensen <mik2650@gmail.com>
2016-05-22 14:19:32 +0200

    Export script now works

--- winescrape.js
+++ winescrape.js
@@ -1,47 +1,21 @@
 (function(){
 
-console.log('title,year,type,rating');
+console.log('title | year | origin_region | origin_contry | rating');
+
 $('.activity-card').each(function(i,ele){
     var title = $(ele).find('.wine-name a.bold').html();
     var vintage = $(ele).find('.wine-name').clone().children().remove().end().text();
+    var origin_region = $(ele).find('.semi a').first().html();
+    var origin_contry = $(ele).find('.semi a').last().html();
+    var avg_rating = $(ele).find('div.wine-rating span.header-large.text-block').html();
+    var avg_price = $(ele).find('.wine-rating .wine-price-value').html();
     
     
-    
-    console.log(title.replace(/\d+/, '').trim() + ',' + vintage.trim());
+    console.log(title.replace(/\d+/, '').trim() 
+        + ' | ' + vintage.trim() 
+        + ' | ' + origin_region.trim() 
+        + ' | ' + origin_contry.trim()
+        + ' | ' + avg_rating.trim()
+        + ' | ' + avg_price.trim());
 });
 })();
-
-
-/*
-
-(function(){
-
-var removeVarietal = function(title){
-    var wine_types = ['port', 'shiraz','merlot', 'sauvignon blanc', 'rosé', 'rose', 'cabernet sauvignon', 'red', 'zinfandel', 'syrah', 'malbec', 'riesling', 'pinot noir', 'cabernet franc', 'bordeaux', 'chardonnay'];
-    var wine_prefix = ['late harvest', 'old vine'];
-    var process_title = {};
-    $(wine_types).each(function(i,val){
-        if (title.indexOf(val) !== -1){
-            process_title['title'] =  title.replace(val,'');
-            process_title['varietal'] = val;
-        }
-    });
-    return process_title;
-}
-
-console.log('title,year,type,rating');
-$('.activity-card').each(function(i,ele){
-    var title = $(ele).find('.wine-info p a').html();
-    var rating = $(ele).find('.mw_buttons input').val();
-    
-    if (title && rating && rating > 0){
-        title = title.toLowerCase();
-        var process_title = removeVarietal(title);
-        console.log(title.replace(/\d+/, '').trim() + ','
-        + title.replace(/[^\d]+/, '')+','
-        +rating );
-
-    }
-});
-})();
-*/
