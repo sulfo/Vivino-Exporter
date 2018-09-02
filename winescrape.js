721bea651fd83bfbd0c6552c7b1c3d38f58b9a6d
jonathankoren <jonathankoren@users.noreply.github.com>
2018-09-02 02:29:07 -0700

    check for missing fields

--- winescrape.js
+++ winescrape.js
@@ -1,20 +1,25 @@
 (function(){
 
-console.log('title | year | origin_region | origin_contry | rating | price');
+console.log('title | year | origin_region | origin_country | rating | price');
 
 $('.activity-card').each(function(i,ele){
     var title = $(ele).find('.wine-name a.bold').html();
+    if (title === undefined) { title = ""; }
     var vintage = $(ele).find('.wine-name').clone().children().remove().end().text();
+    if (vintage === undefined) { vintage = ""; }
     var origin_region = $(ele).find('.semi a').first().html();
-    var origin_contry = $(ele).find('.semi a').last().html();
+    if (origin_region === undefined) { origin_region = ""; }
+    var origin_country = $(ele).find('.semi a').last().html();
+    if (origin_country === undefined) { origin_country = ""; }
     var avg_rating = $(ele).find('div.wine-rating span.header-large.text-block').html();
+    if (avg_rating === undefined) { avg_rating = ""; }
     var avg_price = $(ele).find('.wine-rating .wine-price-value').html();
-    
-    
-    console.log(title.replace(/\d+/, '').trim() 
-        + ' | ' + vintage.trim() 
-        + ' | ' + origin_region.trim() 
-        + ' | ' + origin_contry.trim()
+    if (avg_price === undefined) { avg_price = ""; }
+
+    console.log(title.replace(/\d+/, '').trim()
+        + ' | ' + vintage.trim()
+        + ' | ' + origin_region.trim()
+        + ' | ' + origin_country.trim()
         + ' | ' + avg_rating.trim()
         + ' | ' + avg_price.trim());
 });
